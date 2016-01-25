1ce2d0a90e0b24f8222d90bba389fb5dc1cd0091
Ryan Chong <chongr@umich.edu>
2016-01-25 14:14:36 -0800

    completes first_routes project

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -5,17 +5,36 @@ url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users/1'
+  path: '/users/3'
 ).to_s
+#
+# query_url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users.html',
+#   query_values: {
+#     'var1' => 'val1',
+#     'var2' => 'val2'
+#   }
+# ).to_s
 
-query_url = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users.html',
-  query: 'var1=val1'
-).to_s
-
-puts RestClient.get(url)
+# puts RestClient.get(url)
 # puts RestClient.get(query_url)
 # puts RestClient.post(url, param1: "hi")
+
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { name: 'Gizmo1', email: 'gizmo1@gizmo.gizmo' } }
+  )
+end
+
+puts RestClient.delete(url)
